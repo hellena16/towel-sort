6b3862aeab8e38d6181c9f46a4924200f45f8646
Hellena16 <elena-kuzmitskaya@rambler.ru>
2023-02-27 15:40:28 +0300

    feat: change index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix) {
+    return matrix.map((el, i) => {
+     return  (i % 2 === 0) ? el.sort((a, b) => a - b) : el.sort((a, b) => b - a)
+    }).flat()
+  }
+  else{
+    return []
+  }
 }
